e67536500b366f9e58948faf7d89f29900910a59
Jose M. Torres <jmtorresr@gmail.com>
2020-09-30 12:32:53 +0900

    Attempt to recursive function.
    Co-authored-by: dhequex <dhequex@users.noreply.github.com>
    Co-authored-by: happymanju <happymanju@users.noreply.github.com>

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,5 +1,16 @@
-const getElementsByClassName = () => {
-  // YOUR CODE HERE
+const getElementsByClassName = (targetClassName) => {
+  const element = [];
+  function findClass(htmlString) {
+    let children = htmlString.children;
+
+    if (children === undefined) {
+      return;
+    }
+    if (htmlString.className === targetClassName) {
+      element.push(htmlString);
+    }
+    for (let i = 0; i < children.length; i++) return findClass();
+  }
 };
 
 module.exports = { getElementsByClassName };
